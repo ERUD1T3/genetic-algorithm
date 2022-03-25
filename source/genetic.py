73f0868d4202ccd67c183b4a744c217ed55d4a99
Technocracy <josiasmoukpe@gmail.com>
2022-03-25 13:57:32 -0400

    added read attributes and  read data

--- source/genetic.py
+++ source/genetic.py
@@ -27,9 +27,9 @@ class Genetic:
         '''Initialize the Genetic Algorithm'''
 
         # read params
-        self.training = training
-        self.testing = testing
-        self.attributes = attributes
+        # self.training = training
+        # self.testing = testing
+        # self.attributes = attributes
         self.population = population
         self.mutation = mutation
         self.replacement = replacement
@@ -37,6 +37,16 @@ class Genetic:
         self.fitness_threshold = fitness_threshold
         self.selection_type = selection_type
         self.debug = debug
+
+        # read the data
+        self.attributes, self.order = self.read_attributes(attributes)
+        self.training = self.read_data(training)
+        self.testing = self.read_data(testing)
+
+        if self.debug:
+            print('Attributes: ', self.attributes)
+            print('Order: ', self.order)
+            print('Final Attribute: ', self.order[-1])
 
         # instanting params for the GA
         self.population = []
@@ -47,12 +57,59 @@ class Genetic:
         '''Returns a string representation of the GA'''
         pass
 
+
+    # TODO: test this function
     def read_data(self, path):
         '''Reads data from a file'''
-        pass
+        
+        data = []
 
+        # read in the attributes
+        with open(path, 'r') as f:
+            for line in f:
+                    words = line.strip().split()
+                    data.append(words)
+               
+        if self.debug:
+            print('Read data: ', data)
+
+        if len(data) == 0:
+            raise Exception('No data found')
+
+        return data
+
+     # TODO: test this function
     def read_attributes(self, path):
         '''Reads attributes from a file'''
+        attributes = {}
+        order = []
+
+         # read in the attributes
+        with open(path , 'r') as f:
+            for line in f:
+                if len(line) > 1:
+                    words = line.strip().split()
+                    
+                    # storing the attributes
+                    attributes[words[0]] = words[1:]
+                    order.append(words[0])
+
+                
+        if self.debug:
+            print('Attributes: ', attributes)
+            print('Order: ', order)
+            print('Final Attribute: ', order[-1])
+
+        if len(order) == 0:
+            raise Exception('No attributes found')
+
+
+        return attributes, order
+
+    def encode(self, individual):
+        '''Encodes an individual
+            data instance -> binary encoded instance
+        '''
         pass
 
     def discretize(self, data):
